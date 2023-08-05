362e88b55382fa22fe745758f480bc1bf4a66530
mehedi-h <71249360+mehedi-h@users.noreply.github.com>
2023-08-05 14:21:56 +0600

    Delete js-practise-work directory

--- js-practise-work/case.js
+++
@@ -1,9 +0,0 @@
-const userName = "Mehedi";
-const userId = "hasan";
-
-if (userName.toUpperCase === userId.toUpperCase) {
-    console.log("Valid")
-} else {
-    console.log('Invalid')
-}
-
--- js-practise-work/search.js
+++
@@ -1,5 +0,0 @@
-
-const songs = "Amar Sonar Bangla Ami Tomay Valobashi";
-
-const search = songs.includes('Valobashi')
-console.log(search)
